8a1ce4b5942dbbfa29c1f9577eeb07b09c5176de
Pookbros <joshuapook@gmail.com>
2022-03-15 19:08:51 +0000

    Removed comment

--- server/NewBank.java
+++ server/NewBank.java
@@ -45,8 +45,6 @@ public class NewBank {
 		return null;
 	}
 
-	//Test Comment again
-
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
 		/*Splits input string and checks that the request is in a valid format*/
